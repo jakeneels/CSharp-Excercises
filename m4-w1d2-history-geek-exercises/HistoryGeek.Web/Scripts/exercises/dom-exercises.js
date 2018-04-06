4bbf193f78fd99495f74320f64f741b7b2695107
Aaron Tyler <aaron.tyler@techelevator.com>
2018-04-06 11:05:59 -0400

    Added m1-w1d5 Bootstrap exercises

--- m4-w1d2-history-geek-exercises/HistoryGeek.Web/Scripts/exercises/dom-exercises.js
+++ m4-w1d2-history-geek-exercises/HistoryGeek.Web/Scripts/exercises/dom-exercises.js
@@ -1,48 +1,58 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
 //Locate all <div> elements and add the 'president' class.
 function presentialDivs() {
-
+    $("#presidents div").addClass("president");
 }
 
 //Locate George Washington and add the 'founding-father' class
 function georgeWashington() {
-
+    $("#george-washington").addClass("founding-father");
 }
 
 //Locate all Republican presidents and add the 'red' class.
 function republicanPresidents() {
-
+    $(".Republican").addClass("red");
 }
 
 //Locate all Democrat presidents and add the 'blue' class.
 function democratPresidents() {
-
+    $(".Democratic").addClass("blue");
 }
 
 //Locate all Federalist presidents and add the 'gold' class.
 function otherPresidents() {
-
+    $(".president").not(".Democratic").not(".Republican").addClass("gold");
 }
 
 //Locate all presidents named 'James' and add the 'james' class.
 function presidentsNamedJames() {
-
+    $(".president[id^='james']").addClass('james');
 }
 
 //Locate each president serving at the beginning of each century and add the 'beginningOfCentury' class.
 function turnOfTheCenturyPresidents() {
-
+    $("h2+div").addClass("beginningOfCentury");
 }
 
 //Locate each president serving at the end of the century and add the 'endOfCentury' class.
 function endOfTheCenturyPresidents() {
+    $("h2").prev().addClass("endOfCentury");
+    $(".president").last().addClass("endOfCentury");
     
-  
-
 }
 
 //Locate all presidents serving in the 1900s who appear on currency and add the 'appearOnCurrency' class.
 function currencyPresidents() {
-    
+
+    var items = $(".currency");
+
+    items.each(function (item) {
+        //console.log("item: ", $(item).find("p"))
+        if ($(item).find("p")[0].text().startsWith("(19")) {
+            $(item).addClass("appearOnCurrency");
+            console.log("adding");
+        }
+    })
+
 
 }
