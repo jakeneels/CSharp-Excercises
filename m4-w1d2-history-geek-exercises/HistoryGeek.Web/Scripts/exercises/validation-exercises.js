0c477c799c3ace7403cb7b4d7518a51c0c28dec9
jakeneels <nizlmmk@gmail.com>
2018-04-09 14:10:59 -0400

    tax, event done2

--- m4-w1d2-history-geek-exercises/HistoryGeek.Web/Scripts/exercises/validation-exercises.js
+++ m4-w1d2-history-geek-exercises/HistoryGeek.Web/Scripts/exercises/validation-exercises.js
@@ -2,12 +2,58 @@
 /// <reference path="../jquery.validate.js" />
 
 $(document).ready(function () {
-  $("#payment-info").validate({
+  console.log("valid");
+
+  $("#form0").validate({
+
+    EmailAddress: {
+      email: true,
+      required: true
+    },
+    Password: {
+      required: true,
+      minlength: 8,
+      strongPassword: true
+    },
+    ConfirmPassword: {
+      required: true,
+      equalTo: "#Password"
+    }
 
 
+  })
 
+  $("#checkout").validate({
+    
+    rules: {
 
+      NameOnCard: {
+        required: true,
+        
+      },
+      CreditCardNumber: {
+        required: true,
+        digits: true,
+        minlength: 15,
+        maxlength: 16
+      },
+      ExpirationDate: {
+        minlength: 4,
+        maxlength:5,
+        required: true
+      },
+      messages: {
+        NameOnCard: "Full name required",
+        CreditCardNumber: {
+          minlength: "Invalid number please enter again",
+          maxlength: "Invalid number please enter again"
+        },
+        ExpirationDate: "mm/yy format ex. 01/18 for Jan 2018"
+      },
 
+      errorClass: "error", //css
+      validClass: "valid"
+    }
   });
    
 });
